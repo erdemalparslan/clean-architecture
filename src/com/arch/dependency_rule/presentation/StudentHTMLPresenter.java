de89e54e353436b1b64f10c03cdf059f101ef2f7
erdem-vh <erdem.alparslan@vida.com>
2019-11-14 21:06:24 -0800

    small change

--- src/com/arch/dependency_rule/presentation/StudentHTMLPresenter.java
+++ src/com/arch/dependency_rule/presentation/StudentHTMLPresenter.java
@@ -5,6 +5,6 @@ import com.arch.dependency_rule.entity.Student;
 public class StudentHTMLPresenter {
     public String viewStudentDetails(String id){
         Student student = Student.getStudentFromHashTable(id);
-        return  student.toString() ;
+        return  student.toString();
     }
 }
