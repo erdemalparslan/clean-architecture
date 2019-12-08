22e1c46c4e60277a72a9aafd51c2bf1e7fcc1700
deniz <denizatcali@gmail.com>
2019-12-08 09:20:45 -0800

    more realistic names

--- src/com/arch/Main.java
+++ src/com/arch/Main.java
@@ -1,6 +1,6 @@
 package com.arch;
 
-import com.arch.dependency_rule.drivers.StudentArrayListPersistance;
+import com.arch.dependency_rule.drivers.StudentOraclePersistance;
 import com.arch.dependency_rule.drivers.StudentPersistanceInterface;
 import com.arch.dependency_rule.entity.Student;
 import com.arch.dependency_rule.presentation.StudentHTMLPresenter;
@@ -10,7 +10,7 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-        StudentPersistanceInterface studentPersistance = new StudentArrayListPersistance();
+        StudentPersistanceInterface studentPersistance = new StudentOraclePersistance();
         studentPersistance.save(new Student("A123", "John", new Date(), 3));
         StudentHTMLPresenter presenter = new StudentHTMLPresenter();
         System.out.println(presenter.viewStudentDetails("A123", studentPersistance));
